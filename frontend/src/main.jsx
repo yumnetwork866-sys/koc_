589aff0c334e82180a781e80b996ccc6f96dff81
megumin123578 <saranghae123578@gmail.com>
2026-07-31 14:14:10 +0700

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource-variable/inter'
-import '@fontsource-variable/geist'
 import './styles/index.css'
 import App from './App.jsx'
 
